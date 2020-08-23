48dbf1bce5a4256a8ad0497fb876a66662258901
Whoopinstick <k.m.paul68@gmail.com>
2020-08-23 13:48:19 -0400

    remove image saving ability.  complete image rotation functionality

--- 10 - FlipImage/View/ContentView.swift
+++ 10 - FlipImage/View/ContentView.swift
@@ -9,9 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingImagePicker = false
+    @State private var imageRotation = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15.0)
+                        .fill(image == nil ? Color.secondary : Color.clear)
+                    
+                    //display the image
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                            .rotationEffect(Angle(degrees: imageRotation))
+                    } else {
+                        Text("Tap to select a photo")
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    //select an image
+                    self.showingImagePicker = true
+                }
+                    
+                .padding(.vertical)
+                
+                
+                Button("Rotate") {
+                    self.imageRotation += 90
+                }
+                .disabled(image == nil ? true : false)
+                
+            }
+                
+            .padding([.horizontal, .bottom])
+                
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+                
+            .navigationBarTitle("Image Flip")
+            
+        }
     }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
